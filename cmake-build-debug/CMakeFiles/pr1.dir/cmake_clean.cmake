be60c13a8503b7d0996fea7f4737edf043159b84
angelgg91 <angelgg91@hotmail.com>
2020-10-01 09:07:46 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/pr1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pr1.dir/src/igvEscena3D.cpp.o"
+  "CMakeFiles/pr1.dir/src/igvInterfaz.cpp.o"
+  "CMakeFiles/pr1.dir/src/pr2a.cpp.o"
+  "pr1"
+  "pr1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pr1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/pr2a.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pr2a.dir/main.cpp.o"
+  "pr2a"
+  "pr2a.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pr2a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
